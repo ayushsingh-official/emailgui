6ef479c28a2bdbd3018c23c3753e72993f45e1ee
ayushsingh-official <ayushofficial.in@gmail.com>
2023-06-21 16:10:54 +0530

    Form creation and data object creation by JS

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+
 @Component({
+
   selector: 'app-email',
+
   templateUrl: './email.component.html',
+
   styleUrls: ['./email.component.css']
+
 })
+
 export class EmailComponent {
 
+  constructor(){}
+
+  data={
+    to:"",
+
+    subject:"",
+
+    message:""
+  }
+
+  doSubmitForm()
+  {
+
+    console.log("try to submit form...")
+
+    console.log("DATA ", this.data);
+
+  }
+
+
+
+
 }
+
+
